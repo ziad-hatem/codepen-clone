7e133812e52d4086937a435bb2ceb6d713cc62af
Amir-ashour <109518062+ziad-hatem@users.noreply.github.com>
2023-10-16 10:56:02 +0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+//components
+import Code from './Code';
+import Result from './Result';
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            <Result />
+        </>
+    )
+}
+
+export default Home;
